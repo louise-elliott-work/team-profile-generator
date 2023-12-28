4403a115daa3291d847ac7e5ddab38462a91af85
Louise Elliott <louise.elliott.work@gmail.com>
2023-12-28 17:39:54 +0000

    engineer data capture added

--- index.js
+++ index.js
@@ -3,62 +3,20 @@ const Manager = require("./lib/Manager.js");
 const Engineer = require("./lib/Engineer.js");
 const Intern = require("./lib/Intern.js");
 const inquirer = require("inquirer");
+
+// * file path for html creation in targeted folder
 const path = require("path");
 const fs = require("fs");
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
+// TODO work out how to use this
 const render = require("./src/page-template.js");
-
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
 
 // * Write code in `index.js` that uses inquirer to gather information about the development team members and creates objects for each team member using the correct classes as blueprints.
 
-
-// * array of Team Manager prompts
-// const Manager = [
-//     // * When a user starts the application then they are prompted to enter the **team manager**’s:
-//         //   * Name
-//         //   * Employee ID
-//         //   * Email address
-//         //   * Office number
-//     {
-//         name: 'name',
-//         type: 'input',
-//         message: 'What is the name of your team manager?',
-//     },
-//     {
-//         name: 'id',
-//         type: 'input',
-//         message: 'What is their ID code?',
-//     },
-//     {
-//         name: 'email',
-//         type: 'input',
-//         message: 'What is their email address?',
-//     },
-//     {
-//         name: 'officeNumber',
-//         type: 'input',
-//         message: 'What is their office number?',
-//     },
-//     // * When a user enters those requirements then the user is presented with a menu with the option to:
-//     //   * Add an engineer
-//     //   * Add an intern 
-//     //   * Finish building the team
-//     {
-//         name: 'nextStep',
-//         type: 'list',
-//         message: 'What do you want to do now?',
-//         choices: ['Add an engineer', 'Add an intern', 'Finish building the team']
-//     }
-// ];
-
-
 inquirer
     .prompt ([
-        
             // * When a user starts the application then they are prompted to enter the **team manager**’s:
         //   * Name
         //   * Employee ID
@@ -94,13 +52,44 @@ inquirer
         message: 'What do you want to do now?',
         choices: ['Add an engineer', 'Add an intern', 'Finish building the team']
     }
-
     ])
-    .then((response) => {
-        const { name, id, email, officeNumber, nextStep } = response;
-        console.log(response);
+    .then((Manager) => {
+        const { name, id, email, officeNumber, nextStep } = Manager;
+        console.log(Manager);
         if (nextStep === "Add an engineer") {
             console.log("option to add an engineer selected");
+            inquirer
+                .prompt ([
+                // * When a user selects the **engineer** option then a user is prompted to enter the following and then the user is taken back to the menu:
+                    //   * Engineer's Name
+                    //   * ID
+                    //   * Email
+                    //   * GitHub username
+                {
+                    name: 'name',
+                    type: 'input',
+                    message: 'What is the name of the engineer?',
+                },
+                {
+                    name: 'id',
+                    type: 'input',
+                    message: 'What is their ID code?',
+                },
+                {
+                    name: 'email',
+                    type: 'input',
+                    message: 'What is their email address?',
+                },
+                {
+                    name: 'github',
+                    type: 'input',
+                    message: 'What is their GitHub username?',
+                },
+            ])
+            .then((Engineer) => {
+                const { name, id, email, github } = Engineer;
+                console.log(name, id, email, github);
+            })
         }
         else if (nextStep === "Add an intern") {
             console.log("option to add an intern selected");
@@ -108,7 +97,7 @@ inquirer
         else if (nextStep === "Finish building the team") {
             // * prompt user with questions and log answers in a file named `team.html` in the `output` folder.
             // * When a user decides to finish building their team then they exit the application, and the HTML is generated.   
-            fs.appendFile('output/team.html',
+            fs.appendFile(outputPath,
             (`
                 <div class="card employee-card">
                 <div class="card-header">
@@ -124,38 +113,12 @@ inquirer
                 </div>
             </div>
                 `),
-        (err) => err ? console.error(err) : console.log('README generated')
+        (err) => err ? console.error(err) : console.log('HTML generated')
     )}
 });
 
 
-// // * When a user selects the **engineer** option then a user is prompted to enter the following and then the user is taken back to the menu:
-//     //   * Engineer's Name
-//     //   * ID
-//     //   * Email
-//     //   * GitHub username
-// const EngineerQuestions = [
-//     {
-//         name: 'EngineerName',
-//         type: 'input',
-//         message: 'What is the name of the engineer?',
-//     },
-//     {
-//         name: 'EngineerID',
-//         type: 'input',
-//         message: 'What is their ID code?',
-//     },
-//     {
-//         name: 'EngineerEmail',
-//         type: 'input',
-//         message: 'What is their email address?',
-//     },
-//     {
-//         name: 'EngineerGitHub',
-//         type: 'input',
-//         message: 'What is their GitHub username?',
-//     },
-// ];
+
 
 
 // // * When a user selects the intern option then a user is prompted to enter the following and then the user is taken back to the menu:
@@ -186,8 +149,7 @@ inquirer
 //     },
 // ];
 
-// // * Call the `render` function (provided for you) and pass in an array containing all employee objects; 
-// // * The `render` function will generate and return a block of HTML including templated divs for each employee!
-// // * Create an HTML file using the HTML returned from the `render` function. 
-
-// // * You can use the provided variable `outputPath` to target this location.
+// TODO:
+// Call the `render` function (provided for you) and pass in an array containing all employee objects; 
+// The `render` function will generate and return a block of HTML including templated divs for each employee!
+// Create an HTML file using the HTML returned from the `render` function. 
